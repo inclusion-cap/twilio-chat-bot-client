926013233189939c1439b51aade467ce431bda57
Pavel Machuca <pavelmachuca@hotmail.com>
2019-08-03 15:25:12 -0400

    added footer

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import Header from './components/Header'
 import Body from './components/Body'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
       <Header className="App-header" />
       <Body />
+      <Footer />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,11 @@
+import React from 'react';
+
 function Header() {
-  return null;
+  return (
+    <div id="header">
+      <h1>Scoops Ah<span><img id="header-wheel" src="https://cdn.pixabay.com/photo/2013/07/13/10/47/ship-157789_960_720.png" alt='o' /></span>y</h1>
+    </div>
+  );
 }
 
 export default Header;
